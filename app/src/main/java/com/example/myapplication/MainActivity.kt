6984125c15c5a6d01c416ed4a9fcd38be05b2c62
Anders Schou <andes21@student.sdu.dk>
2024-04-03 10:08:05 +0200

    Dialog box works 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,48 +1,28 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -51,19 +31,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.ui.theme.Purple40
 import com.example.myapplication.ui.theme.Purple80
-import kotlin.random.Random
-import androidx.compose.ui.ExperimentalComposeUiApi
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
@@ -142,6 +117,24 @@ class PreferencesManager(context: Context) {
     }
     //emilkode//
 
+    //andersKode//
+    fun saveFirstlogin(firstLogin: Boolean) {
+        val editor=sharedPreferences.edit()
+        editor.putInt("FIRST_LOGIN", firstLogin)
+        editor.apply()
+    }
+
+    fun getFirstLogin(): Boolean {
+        return sharedPreferences.getBoolean("FIRST_LOGIN", true)
+    }
+    //forsøg 3//
+
+
+            //andersKode//
+}
+
+private fun SharedPreferences.Editor.putInt(s: String, firstLogin: Boolean) {
+    TODO("Not yet implemented")
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -149,11 +142,12 @@ class PreferencesManager(context: Context) {
 @ExperimentalAnimationApi
 @Composable
 fun MyApp() {
-    var welcomeShown by remember { mutableStateOf(true) }
+    var saveFirstLogin by remember { mutableStateOf(true) }
 
-    if (welcomeShown) {
-        WelcomePopup(onDismiss = { welcomeShown = false })
+    if (saveFirstLogin) {
+        WelcomePopup(onDismiss = { saveFirstLogin = false })
     } else
+
     {
         //Toggle variables for all 4 expand buttons
         var isExpandedB1 = rememberSaveable {
@@ -373,6 +367,7 @@ fun WelcomePopup(onDismiss: () -> Unit) {
             Button(onClick = onDismiss) {
 
                 Text("Forstået")
+
             }
         }
     )
