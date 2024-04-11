bc078b152b8ae00212474b0fc66d183a224d4b3c
Mads Jensen <madsj20@student.sdu.dk>
2024-04-11 12:58:43 +0200

    Added scalable achievement system

--- app/src/main/java/com/example/myapplication/TopBar.kt
+++ app/src/main/java/com/example/myapplication/TopBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -26,6 +27,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,18 +35,27 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.PopupProperties
+import com.example.myapplication.ui.theme.Purple40
+import com.example.myapplication.ui.theme.Purple80
+import com.example.myapplication.ui.theme.PurpleGrey80
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -126,7 +137,12 @@ fun CustomTopBar() {
 fun Popup(openDialog: MutableState<Boolean>,isHeldDown: MutableState<Boolean>){
     val cornerSize = 10.dp //box specs
     val coroutineScope = rememberCoroutineScope()
-    val achievements = listOf("Achivments1", "Achivments2", "Achivments3", "Achivments4")
+    data class Achievement(val name: String, val currentProgress: Int, val goalProgress: Int)
+    val achievements = remember {mutableStateListOf(
+        Achievement("Achievement 1", 50, 100),
+        Achievement("Achievement 2", 70, 200),
+        Achievement("Achievement 3", 30, 150)
+    ) }
 
     androidx.compose.ui.window.Popup(
         alignment = Alignment.Center, //here we mention the pos
@@ -148,12 +164,12 @@ fun Popup(openDialog: MutableState<Boolean>,isHeldDown: MutableState<Boolean>){
                     .fillMaxHeight(0.8f)
                     .padding(top = 2.dp)
                     .background(
-                        Color.Green,
+                        Purple80,
                         RoundedCornerShape(cornerSize)
                     )
                     .border(
-                        1.dp,
-                        Color.Black,
+                        5.dp,
+                        Purple40,
                         RoundedCornerShape(cornerSize)
                     )
             ) {
@@ -163,42 +179,74 @@ fun Popup(openDialog: MutableState<Boolean>,isHeldDown: MutableState<Boolean>){
                     verticalArrangement = Arrangement.spacedBy(5.dp)
 
                 ) {
-                    /*for (achievement in achievements) {
-                        Text(
-                            text = achievement,
-                            modifier = Modifier.padding(vertical = 5.dp),
-                            fontSize = 16.sp
-                        )
-                        Divider(color = Color.Black, thickness = 1.dp)
-                    }*/
-                    /*Text(
-                        text = "Achivments1",
-                        modifier = Modifier.padding(vertical = 5.dp),
-                        fontSize = 16.sp
-                    )
 
-                    Divider(color = Color.Black, thickness = 1.dp)
+                    for (achievement in achievements) {
+                        Column {
+                            Text(
+                                text = achievement.name,
+                                modifier = Modifier.padding(vertical = 5.dp),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold
+                                )
+                            Box(
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                LinearProgressIndicator(
+                                    progress = achievement.currentProgress.toFloat() / achievement.goalProgress, // Calculate the progress
+                                    color = Purple40, // Customize the color of the progress bar
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(20.dp)
+                                        .clip(RoundedCornerShape(5.dp)) // Apply rounded corners
 
-                    Text(
-                        text = "Achivments2",
-                        modifier = Modifier.padding(vertical = 5.dp),
-                        fontSize = 16.sp
-                    )
-
-                    Divider(color = Color.Black, thickness = 1.dp)
-
-                    Text(
-                        text = "Achivments3",
-                        modifier = Modifier.padding(vertical = 5.dp),
-                        fontSize = 16.sp
-                    )
-
-                    Divider(color = Color.Black, thickness = 1.dp)*/
+                                )
+                                Text(
+                                    text = "${achievement.currentProgress}/${achievement.goalProgress}",
+                                    modifier = Modifier
+                                        .padding(vertical = 5.dp)
+                                        .wrapContentHeight(align = Alignment.CenterVertically),
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black,
+                                )
+                                //Text contour
+                                Text(
+                                    text = "${achievement.currentProgress}/${achievement.goalProgress}",
+                                    modifier = Modifier
+                                        .padding(vertical = 5.dp)
+                                        .wrapContentHeight(align = Alignment.CenterVertically),
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.White,
+                                    style = TextStyle.Default.copy(
+                                        fontSize = 18.sp,
+                                        drawStyle = Stroke(
+                                            miter = 50f,
+                                            width = 3f,
+                                            join = StrokeJoin.Round
+                                        )
+                                    )
+                                )
+                            }
+                            Divider(
+                                color = Color.Black,
+                                thickness = 2.dp,
+                                modifier = Modifier.padding(vertical = 5.dp)
+                                )
+                        }
+                    }
+                    Button(onClick = {
+                        achievements.add(Achievement("New Achievement", 0, 100))
+                    }) {
+                        Text("Add Achievement")
+                    }
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun Info(onDismiss: () -> Unit){
